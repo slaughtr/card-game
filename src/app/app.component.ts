e0b142a7098a66e7b2f657f39ab80ec2bec23b1c
Chris Carr <coder.knot@gmail.com>
2017-05-15 11:55:08 -0700

    test for card service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Router } from "@angular/router";
+
+import { CardService } from "./card.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CardService]
 })
 
 export class AppComponent {
   title = 'app works!';
+  cards: FirebaseListObservable<any[]>;
 
-  constructor(private router: Router) {}
+
+  constructor(private router: Router, private cardService: CardService) {}
+
+  ngOnInit() {
+    this.cards = this.cardService.getCards();
+  }
 
   goToPlayerDetailPage(currentPlayer) {
     this.router.navigate(['player', currentPlayer.$key])
