3f484ca1acc050c76a2b39dd4876fba6a88807b1
slaughtr <dslaughtr@gmail.com>
2017-05-15 11:08:51 -0700

    Add app.routing, boilerplate for app.component

---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainPageComponent } from './main-page/main-page.component'
+import { PlayerDetailComponent } from './player-detail/player-detail.component'
+import { AboutComponent } from './about/about.component'
+import { HelpComponent } from './help/help.component'
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent
+  },
+  {
+    //TODO: player info, anything useful here will necessitate adding properties to player object (IE wins/losses)
+    path: 'player/:id',
+    component: PlayerDetailComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'help',
+    component: HelpComponent
+  }
+]
