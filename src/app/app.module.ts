511a092aee9e2919994579b347b1e0fbe7a5b2c3
slaughtr <dslaughtr@gmail.com>
2017-05-16 08:18:53 -0700

    Cleanup of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {AngularFireModule} from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import {AngularFireAuthModule} from 'angularfire2/auth'
-import { masterFirebaseConfig } from './api-keys';
-import {AuthService} from './providers/auth.service';
-import { CardService } from './card.service'
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './providers/auth.guard';
+
+//services, etc
+import { CardService } from './card.service'
+import { routing } from './app.routing';
+
+//components
 import { MainPageComponent } from './main-page/main-page.component';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { AboutComponent } from './about/about.component';
@@ -19,11 +20,16 @@ import { HandComponent } from './hand/hand.component';
 import { DeckComponent } from './deck/deck.component';
 import { LaneComponent } from './lane/lane.component';
 import { CardComponent } from './card/card.component';
-import { routing } from './app.routing';
 import { GameComponent } from './game/game.component';
 import { DiscardComponent } from './discard/discard.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {AuthGuard} from './providers/auth.guard';
+
+//firebase
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth'
+import {AuthService} from './providers/auth.service';
+import { masterFirebaseConfig } from './api-keys';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
