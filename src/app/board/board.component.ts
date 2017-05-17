7686e7d11f41d553f2645828a82fcdf6ad924791
slaughtr <dslaughtr@gmail.com>
2017-05-17 16:03:26 -0700

    Fix board.component, add hand.component back into board.component.html

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -17,58 +17,33 @@ import { EnemyLaneService } from '../enemy-lane.service'
 })
 
 export class BoardComponent implements OnInit {
-
+  player;
+  wizardPlayer;
+  piratePlayer;
   currentUser: any;
   game: any;
   playerIsPirate: boolean;
   pirateDeck: any = [];
 
-  lanes: number[];
-  constructor(private playerService: PlayerService, private authService: AuthService, private gameService: GameService) { }
+  constructor(private playerService: PlayerService, private playCardService: PlayCardService, private enemyLaneService: EnemyLaneService, private authService: AuthService, private gameService: GameService) { }
 
   ngOnInit() {
-    this.authService.getCurrentUser().subscribe(user =>  {
-      this.currentUser = user;
-
-        });
-      this.gameService.getGame().subscribe((game)=> {
-        this.game = game;
-        if (this.game.Pirate === this.currentUser.displayName){
-          this.playerIsPirate = true;
-        }
-      });
-    this.playerService.getPlayerById("1").subscribe(player => this.lanes = player.lanes)
-    // this.playerService.getPlayerById("1").subscribe(player => player.lanes.map(lane => this.lanes.push(lane)))
-
-  player;
-
-  constructor(private playerService: PlayerService, private playCardService: PlayCardService, private enemyLaneService: EnemyLaneService) { }
-  wizardPlayer;
-  piratePlayer;
-
-
-
-  ngOnInit() {
-    let currentPlayer = this.playerService.getPlayerById("1").subscribe((player)=> {
-      this.player = player
-    }
-  )
+    // let currentPlayer = this.playerService.getPlayerById("1").subscribe((player)=> {
+    //   this.player = player
+    // })
     //this function loads cards already played on init. Afterwards, players should already be subscribed to the played cards, so not necessary afterwards?
     this.playCardService.getPlayedCards()
     this.enemyLaneService.getEnemyLanes()
-
-
   }
+
   getPirateDeck() {
     this.pirateDeck = []
     this.gameService.getPirateDeck().subscribe(deck => {
-        deck.cards.forEach(card=> {
-            this.pirateDeck.push(card);
+      deck.cards.forEach(card=> {
+        this.pirateDeck.push(card);
       });
     })
     this.playerService.savePlayerDeck(this.pirateDeck);
   }
-
-
 
 }
