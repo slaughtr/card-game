c6fcf7b7cf25d51b4fa7528822d21a801f435b95
Chris Carr <coder.knot@gmail.com>
2017-05-17 16:52:46 -0700

    add method to add card to discard pile via game service

--- src/app/discard/discard.component.ts
+++ src/app/discard/discard.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { GameService } from '../game.service';
+import { PlayerService } from '../player.service';
+import { Card } from '../card.model';
 
 @Component({
   selector: 'app-discard',
   templateUrl: './discard.component.html',
-  styleUrls: ['./discard.component.css']
+  styleUrls: ['./discard.component.css'],
+  providers: [GameService, PlayerService]
 })
-export class DiscardComponent implements OnInit {
 
-  constructor() { }
+export class DiscardComponent implements OnInit {
+  cards: FirebaseListObservable<any[]>;
+  discard;
+
+  constructor(private database: AngularFireDatabase, private gameService: GameService, playerService: PlayerService) {  }
 
   ngOnInit() {
+    this.gameService.getDiscard().subscribe((discard) => {
+      this.discard = discard;
+    })
+  }
+
+  addToDiscard() {
+    var testCard: Card = new Card("Pirate Boatswain", 10, 5, "boatswaining");
+    this.gameService.addToDiscard(testCard);
   }
 
 }
