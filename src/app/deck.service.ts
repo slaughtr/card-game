6997905e512947bd7b45bac60031e70e2c514c1e
Keith Stedman <keithstedman@gmail.com>
2017-05-17 13:03:10 -0700

    deck assign and shuffle complete

--- src/app/deck.service.ts
+++ src/app/deck.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class DeckService {
@@ -10,7 +10,7 @@ export class DeckService {
   }
 
   shuffleDeck(){
-    
+
   }
 
   getDecks() {
--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -2,28 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../player.service';
 import { AuthService } from '../providers/auth.service';
 import { DeckService } from '../deck.service';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-deck',
   templateUrl: './deck.component.html',
   styleUrls: ['./deck.component.css'],
-  providers: [PlayerService]
+  providers: [PlayerService, DeckService]
 })
 
 
 export class DeckComponent implements OnInit {
-player;
+  decks: FirebaseListObservable<any[]>;
+  player;
+  decksToDisplay;
+  pirateDeck;
+  pirateCards;
+  pirateShuffled;
+  wizardDeck;
+  wizardCards;
+  wizardShuffled;
 
-  constructor(private playerService: PlayerService, private deckService: DeckService, private authService: AuthService) { }
+  constructor(private playerService: PlayerService, private deckService: DeckService, private database: AngularFireDatabase) { }
 
   ngOnInit() {
     let currentPlayer = this.playerService.getPlayerById("1").subscribe((player)=> {
       this.player = player;
     });
 
-    let shuffledDecks = this.deckService.getDecks().subscribe((deck) => {
-      console.log(shuffledDecks);
+    this.deckService.getDecks().subscribe(decks => {
+      this.pirateDeck = decks[0];
+      this.wizardDeck = decks[1];
+      this.pirateCards = this.pirateDeck.cards;
+      this.wizardCards = this.wizardDeck.cards;
+
+      this.pirateShuffled = this.shuffleDeck(this.pirateDeck);
+      this.wizardShuffled = this.shuffleDeck(this.wizardDeck);
+      console.log("end of subscribe");
     });
+
+  }
+
+  shuffleDeck(deckToShuffle){
+    var initDeck = deckToShuffle.cards;
+    var loopLength = deckToShuffle.cards.length;
+    var outputDeck = new Array();
+    var randomCard;
+    for(var i = 0; i < loopLength; i++){
+      var randomIndex = Math.floor(Math.random()*initDeck.length);
+      randomCard = initDeck[randomIndex];
+      outputDeck.push(randomCard);
+      initDeck.splice(randomIndex,1);
+    }
+    return outputDeck;
   }
 
 }
